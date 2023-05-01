f8f1cb243e5e9e71ae5f184f13aafd4338ec792c
Guahuan <zjukyj@163.com>
2023-05-02 00:35:38 +0800

    完成master connection

--- Distributed-Client/src/Components/Client.java
+++ Distributed-Client/src/Components/Client.java
@@ -1,29 +1,35 @@
 package Components;
 
+import Connection.MasterConnection;
+import Connection.RegionConnection;
+
 public class Client {
     private String MasterIP;
     private int MasterPort;
     private Cache cache;
+    private MasterConnection master;
+    private RegionConnection region;
 
     public Client() {
         cache = new Cache();
     }
 
     public void GetMaster() {
-        // TODO: get master info from zookeeper
-        MasterIP = "localhost";
+        // TODO: connect to zookeeper and get master info
+        MasterIP = "127.0.0.1";
         MasterPort = 8090;
     }
 
     public void run() {
-        // get master info from zookeeper
+        // connect to zookeeper and get master info
         try {
             GetMaster();
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
-            e.printStackTrace(System.err);
+            return;
         }
-        
+        // connect to master
+        master = new MasterConnection(MasterIP, MasterPort);
 
     }
 
--- Distributed-Client/src/Connection/MasterConnection.java
+++ Distributed-Client/src/Connection/MasterConnection.java
@@ -1,5 +1,77 @@
 package Connection;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class MasterConnection {
+    private String ip;
+    private int port;
+    private Socket socket;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
+    public MasterConnection(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String IP() {
+        return ip;
+    }
+
+    public int Port() {
+        return port;
+    }
+
+    public String toString() {
+        return ip + ":" + port;
+    }
+
+    public boolean connect() {
+        try {
+            socket = new Socket(ip, port);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            return true;
+        } catch (IOException e) {
+            System.err.println("Failed to connect to the master server: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean send(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+            return true;
+        } catch (IOException e) {
+            System.err.println("Failed to send message to the master server: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public String receive() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            System.err.println("Failed to receive message from the master server: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean close() {
+        try {
+            socket.close();
+            return true;
+        } catch (IOException e) {
+            System.err.println("Failed to close the connection to the master server: " + e.getMessage());
+            return false;
+        }
+    }
 
 }
