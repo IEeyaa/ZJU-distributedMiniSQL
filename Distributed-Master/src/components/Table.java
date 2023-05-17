2494089556df7c3e5fbd40340be98c10bc316069
Dian-S <957085525@qq.com>
2023-05-17 23:21:42 +0800

    5.17 Shen fix some bugs

--- Distributed-Master/src/components/Table.java
+++ Distributed-Master/src/components/Table.java
@@ -197,7 +197,7 @@ public class Table {
      * Output: none
      */
     public void removeRegion(String ip) {
-        if(ipToTables.get(ip) == null)
+        if(ipToTables.get(ip) == null || ipToSocket.get(ip) == null)
             return;
         for (String i : ipToTables.get(ip)) {
             String anotherIP = selectExcept(ip);
